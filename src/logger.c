de6d62908f11830502ea6ce8cfa1572c145ee381
Sergey Zolotarev <sryze@protonmail.com>
2020-06-21 23:45:02 +0600

    Update year

--- src/logger.c
+++ src/logger.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Sergey Zolotarev
+ * Copyright (c) 2019-2020 Sergey Zolotarev
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
